3835ba6d00f32aaf6aaba81799a784cda38617e5
netinho537ia <netinhooo.537@gmail.com>
2026-01-15 11:51:34 -0300

    atualização server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import app from './app.js';
 import './database/index.js';
 import 'dotenv/config';
 
-const port = process.env.PORT;
-app.listen(port, () => {
+const port = process.env.PORT || 3001;
+app.listen(port, '0.0.0.0', () => {
 	console.log('Server is running on port 3001');
 });
 
