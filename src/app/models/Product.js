2df7a3f099b388b1c87b22f1015d94cdcaeee1ef
netinho537ia <netinhooo.537@gmail.com>
2026-02-13 10:30:19 -0300

    fix: cloudinary peer dependency compatibility

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -9,15 +9,6 @@ class Product extends Model {
 				image_url: Sequelize.STRING,
 				public_id: Sequelize.STRING,
 				offer: Sequelize.BOOLEAN,
-<<<<<<< HEAD
-=======
-				url: {
-					type: Sequelize.VIRTUAL,
-					get() {
-						return `https://dev-burguer-backend.xgg4n8.easypanel.host/product-file/${this.path}`;
-					},
-				},
->>>>>>> 12a2a8722dd6cdd380cb405f19a7ed71ea56a287
 			},
 			{
 				sequelize,
--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v1 as cloudinary } from 'cloudinary';
 import 'dotenv/config';
 
 cloudinary.config({
